c9a92ff96ccb62d8b8699d125fc9fd050c0aefea
Tony LX1 Cao <caolx1@lenovo.com>
2017-04-16 23:07:02 +0800

    首页PC版初始化

---
+++ app/js/app.js
@@ -0,0 +1,14 @@
+/**
+ * Created by caoliuxue on 2017/4/16.
+ */
+$(document).ready(function () {
+    var mySwiper = new Swiper('.swiper-container', {
+        // Optional parameters
+        direction: 'horizontal',
+        loop: true,
+        speed: 3000,
+        // autoplay: true,
+        paginationHide:false,
+        pagination: '.swiper-pagination'
+    });
+});
